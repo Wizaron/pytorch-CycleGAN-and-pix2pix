5b1db3acd8b29af446bfa1bc6e5cc1e5a047fbbc
Onur Kaplan <kaplanonu@gmail.com>
2019-06-06 00:19:13 +0300

    Single direction prediction

--- scripts/test_animated_video.sh
+++ scripts/test_animated_video.sh
@@ -4,4 +4,4 @@ CHECKPOINTS_DIR=${3}
 GPU_ID=${4}
 
 set -ex
-python test.py --dataroot ${DATA} --name animated-video --model cycle_gan --phase test --no_dropout --no_flip --load_size 500 --preprocess scale_height --checkpoints_dir ${CHECKPOINTS_DIR} --gpu_ids ${GPU_ID} --results_dir ${OUTPUT}
+python test.py --dataroot ${DATA} --name animated-video --model test --phase test --no_dropout --no_flip --load_size 500 --preprocess scale_height --checkpoints_dir ${CHECKPOINTS_DIR} --gpu_ids ${GPU_ID} --results_dir ${OUTPUT} --dataset_mode single
