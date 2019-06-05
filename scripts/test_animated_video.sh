881d8ba19e3e502d46b5d2b21a3d10837e20663d
Onur Kaplan <kaplanonu@gmail.com>
2019-06-05 23:47:58 +0300

    Add no_flip to test_animated_video.sh

--- scripts/test_animated_video.sh
+++ scripts/test_animated_video.sh
@@ -1,8 +1,7 @@
 DATA=${1}
 OUTPUT=${2}
 CHECKPOINTS_DIR=${3}
-AR=${4}
-GPU_ID=${5}
+GPU_ID=${4}
 
 set -ex
-python test.py --dataroot ${DATA} --name animated-video --model cycle_gan --phase test --no_dropout --load_size 500 --preprocess scale_height --checkpoints_dir ${CHECKPOINTS_DIR} --gpu_ids ${GPU_ID} --results_dir ${OUTPUT} --aspect_ratio ${AR}
+python test.py --dataroot ${DATA} --name animated-video --model cycle_gan --phase test --no_dropout --no_flip --load_size 500 --preprocess scale_height --checkpoints_dir ${CHECKPOINTS_DIR} --gpu_ids ${GPU_ID} --results_dir ${OUTPUT}
