2c57c26082d6121259ba2cc983bb6f02d1a3b9bf
Onur Kaplan <kaplanonu@gmail.com>
2019-06-07 13:01:33 +0300

    Add aspect_ratio & disable iff aspect_ratio == -1.0

--- util/visualizer.py
+++ util/visualizer.py
@@ -36,12 +36,13 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
         im = util.tensor2im(im_data)
         image_name = '%s_%s.png' % (name, label)
         save_path = os.path.join(image_dir, image_name)
-        #h, w, _ = im.shape
-        #im_ar = float(w) / h
-        #if aspect_ratio > 1.0:
-        #    im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
-        #if aspect_ratio < 1.0:
-        #    im = imresize(im, (int(h / aspect_ratio), w), interp='bicubic')
+        h, w, _ = im.shape
+        if aspect_ratio == -1.0:
+            pass
+        elif aspect_ratio > 1.0:
+            im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
+        elif aspect_ratio < 1.0:
+            im = imresize(im, (int(h / aspect_ratio), w), interp='bicubic')
         util.save_image(im, save_path)
 
         ims.append(image_name)
